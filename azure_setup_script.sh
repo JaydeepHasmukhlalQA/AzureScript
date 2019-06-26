8ba81b5a73d769246f7b422dc9f8b934d952f811
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-06-26 16:04:09 +0100

    Fixed an error where NSG rule wasn't working and typos

--- azure_setup_script.sh
+++ azure_setup_script.sh
@@ -18,10 +18,10 @@ az network vnet create --resource-group MainResourceGroup --name MainVirtualNetw
 #Creating Network Security Group with port rules:
 az network nsg create --resource-group MainResourceGroup --name NetworkSecurityGroup
 
-az network nsg rule create --resource-group MainResourceGroup --name SSH --destination-port-ranges 22 -- priority 100 --nsg-name NetworkSecurityGroup
-az network nsg rule create --resource-group MainResourceGroup --name HTTP --destination-port-ranges 80 -- priority 101 --nsg-name NetworkSecurityGroup
-az network nsg rule create --resource-group MainResourceGroup --name HTTPS --destination-port-ranges 443 -- priority 102 --nsg-name NetworkSecurityGroup
-az network nsg rule create --resource-group MainResourceGroup --name JenkFly --destination-port-ranges 8080 -- priority 103 --nsg-name NetworkSecurityGroup
+az network nsg rule create --resource-group MainResourceGroup --nsg-name NetworkSecurityGroup --name SSH --destination-port-ranges 22 --priority 100
+az network nsg rule create --resource-group MainResourceGroup --nsg-name NetworkSecurityGroup --name HTTP --destination-port-ranges 80 -- priority 101
+az network nsg rule create --resource-group MainResourceGroup --nsg-name NetworkSecurityGroup --name HTTPS --destination-port-ranges 443 -- priority 102
+az network nsg rule create --resource-group MainResourceGroup --nsg-name NetworkSecurityGroup --name JenkFly --destination-port-ranges 8080 -- priority 103
 
 #Creating public IP for Jenkins and Wildfly:
 az network public-ip create --resource-group MainResourceGroup --name JenkinsIP --dns-name jenkinsvm
@@ -32,5 +32,5 @@ az network nic create --resource-group MainResourceGroup --name JenkinsNetworkIn
 az network nic create --resource-group MainResourceGroup --name WildFlyNetworkInterface --vnet-name MainVirtualNetwork --subnet VirtualNetworkSubnet --network-security-group NetworkSecurityGroup --public-ip-address WildflyIP
 
 #Creating Jenkins and Wildfly VM:
-az vm create --resource-group MainResourceGroup --name JenkinsVirtualMachine --image UbuntuLTS --size Standard_B1s --nic JenkinsNetworkInterface --admin-username jenkins --admin-password Password1234 jenkins --generate-ssh-keys
-az vm create --resource-group MainResourceGroup --name WildflyVirtualMachine --image UbuntuLTS --size Standard_B1s --nic WildFlyNetworkInterface --admin-username wildfly --admin-password Password1234 wildfly --generate-ssh-keys
+az vm create --resource-group MainResourceGroup --name JenkinsVirtualMachine --image UbuntuLTS --size Standard_B1s --nic JenkinsNetworkInterface --admin-username jenkins --admin-password Password1234 --generate-ssh-keys
+az vm create --resource-group MainResourceGroup --name WildflyVirtualMachine --image UbuntuLTS --size Standard_B1s --nic WildFlyNetworkInterface --admin-username wildfly --admin-password Password1234 --generate-ssh-keys
